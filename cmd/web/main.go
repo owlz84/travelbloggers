94fb6be6c1f598133076f668b42a9aebcf27c28b
Stuart Lynn <owlz84@users.noreply.github.com>
2023-03-05 10:34:07 +0000

    switch to http

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -87,7 +87,8 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", addr)
-	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
+	err = srv.ListenAndServe()
+	//err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
